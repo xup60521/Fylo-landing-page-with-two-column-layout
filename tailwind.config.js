10888ddbdd92151734b079501727d488c9af248e
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-31 21:42:05 +0800

    desktop simple layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Very_Dark_Blue: "hsl(243, 87%, 12%)",
+                c_Desaturated_Blue: "hsl(238, 22%, 44%)",
+                c_Bright_Blue: "hsl(224, 93%, 58%)",
+                c_Moderate_Cyan: "hsl(170, 45%, 43%)",
+                c_Light_Grayish_Blue: "hsl(240, 75%, 98%)",
+                c_Light_Gray: "hsl(0, 0%, 75%)",
             },
             fontFamily: {
-
+                paragraph_sans: ["Open Sans", "sans-serif"],
+                heading_btn_nav: ["Raleway", "system-ui"]
             }
         },
     },
